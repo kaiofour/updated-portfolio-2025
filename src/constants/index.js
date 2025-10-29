30da448c8d465cc2701ba0d8ad76f9ce5493a3a0
KURT.CARRILLO <23101122@usc.edu.ph>
2025-10-30 06:00:23 +0800

    Initial commit

---
+++ src/constants/index.js
@@ -0,0 +1,222 @@
+export const myProjects = [
+  {
+    id: 1,
+    title: "Music Platform: Spotify Clone",
+    description:
+      "Music Streaming Platform – UI/UX Recreation of Core Features",
+    subDescription: [
+      "Recreated Spotify's core features playlists, dynamic-form submissions, and user login using React Native and Expo.",
+      "Designed a sleek, responsive UI to mirror Spotify's user experience.",
+      "Implemented navigation with smooth transitions, animations, and controls.",
+    ],
+    image: "/assets/projects/spotify-app.jpg",
+    href: "https://github.com/kaiofour/IT-5103---Advanced-Mobile/tree/main/spotify",
+    tags: [
+      {
+        id: 1,
+        name: "React-Native",
+        path: "/assets/logos/react.svg",
+      },
+      {
+        id: 2,
+        name: "Expo",
+        path: "/assets/logos/expo.svg",
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: "Fitness App: FitTrack Pro",
+    description:
+      "A comprehensive fitness tracking web application with user authentication and data management.",
+    subDescription: [
+      "Implemented role-based access control (RBAC) for fine-grained user permissions.",
+      "Developed a Sleek frontend with Boostrap CSS for a sleek user experience.",
+      "Connected to a secure MySQL database for user data storage.",
+    ],
+    href: "https://github.com/kaiofour/fittrack_final",
+    logo: "",
+    image: "/assets/projects/fittrack-pro.jpg",
+    tags: [
+      {
+        id: 1,
+        name: "php",
+        path: "/assets/logos/php.svg",
+      },
+      {
+        id: 2,
+        name: "mysql",
+        path: "/assets/logos/mysql.svg",
+      },
+      {
+        id: 3,
+        name: "bootstrap",
+        path: "/assets/logos/bootstrap.svg",
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: "Talent Management App: Golden Light",
+    description:
+      "Golden Light is a platform that is dedicated to nurturing talent and fostering success in the vibrant worlds of fashion, beauty, and entertainment.",
+    subDescription: [
+      "Designed responsive UI components with Boostrap CSS for an enhanced UX.",
+      "Handled form submissions and user authentication using PHP.",
+      "Integrated MySQL for efficient client-side database storage.",
+    ],
+    href: "https://github.com/kaiofour/WebdevFinal",
+    logo: "",
+    image: "/assets/projects/golden-light.jpg",
+    tags: [
+      {
+        id: 1,
+        name: "php",
+        path: "/assets/logos/php.svg",
+      },
+      {
+        id: 2,
+        name: "bootstrap",
+        path: "/assets/logos/bootstrap.svg",
+      },
+      {
+        id: 3,
+        name: "MySQL",
+        path: "/assets/logos/mysql.svg",
+      },
+      {
+        id: 4,
+        name: "html5",
+        path: "/assets/logos/html5.svg",
+      },
+    ],
+  },
+  {
+    id: 4,
+    title: "Clothing Brand E-Commerce Platform: Gestourage",
+    description:
+      "A scalable e-commerce platform for a clothing brand, featuring seamless UI and robust backend integration.",
+    subDescription: [
+      "Implemented UI components using BootstrapVue for a responsive shopping experience.",
+      "Followed UI/UX best practices to ensure a user-friendly interface.",
+      "Built Product Management and Order Processing APIs using Laravel.",
+      "Integrated with Sales Module for real-time inventory updates.",
+    ],
+    href: "https://github.com/kaiofour/Gestourage-Admin-Product-CRUD-Panel",
+    logo: "",
+    image: "/assets/projects/gestourage.jpg",
+    tags: [
+      {
+        id: 1,
+        name: "Laravel",
+        path: "/assets/logos/laravel.svg",
+      },
+      {
+        id: 2,
+        name: "BootstrapVue",
+        path: "/assets/logos/bootstrap.svg",
+      },
+      {
+        id: 3,
+        name: "html5",
+        path: "/assets/logos/html5.svg",
+      },
+      {
+        id: 4,
+        name: "",
+        path: "/assets/logos/css.svg",
+      },
+    ],
+  },
+];
+
+export const mySocials = [
+  {
+    name: "WhatsApp",
+    href: "https://wa.me/639565635799",
+    icon: "/assets/socials/whatsApp.svg",
+  },
+  {
+    name: "Linkedin",
+    href: "https://www.linkedin.com/in/kurt-russel-edullantes-carrillo-480524286/",
+    icon: "/assets/socials/linkedIn.svg",
+  },
+  {
+    name: "Instagram",
+    href: "https://www.instagram.com/r.ed884/",
+    icon: "/assets/socials/instagram.svg",
+  },
+];
+
+export const experiences = [
+  {
+    title: "Software Developer",
+    job: "Clothing Brand E-Commerce Platform",
+    date: "2024",
+    contents: [
+      "Designed and implemented the digital infrastructure for a clothing brand's e-commerce platform, focusing on scalable user experience and operational efficiency.",
+      "Developed robust APIs and seamless front-end integration to power core business functions, including inventory management, product visualization, and secure checkout.",
+      "Seamless UI Implementation: Utilized modern component architecture (e.g., Bootstrap/Vue) to create a frictionless, responsive, and platform-agnostic shopping experience.",
+      "Scalable API Development: Created RESTful APIs to handle backend integration, ensuring product catalog loading and transaction processing for financial integrity.",
+    ],
+  },
+  {
+    title: "Freelance Developer",
+    job: "Self-Employed",
+    date: "2025-Present",
+    contents: [
+      "Created a personal portfolio using React and Vite, to showcase technical expertise.",
+      "Continuously enhancing technical skills and expanding expertise in modern web development and back-end technologies.",
+    ],
+  },
+];
+export const reviews = [
+  {
+    name: "Jack",
+    username: "@jack",
+    body: "I've never seen anything like this before. It's amazing. I love it.",
+    img: "https://robohash.org/jack",
+  },
+  {
+    name: "Jill",
+    username: "@jill",
+    body: "I don't know what to say. I'm speechless. This is amazing.",
+    img: "https://robohash.org/jill",
+  },
+  {
+    name: "John",
+    username: "@john",
+    body: "I'm at a loss for words. This is amazing. I love it.",
+    img: "https://robohash.org/john",
+  },
+  {
+    name: "Alice",
+    username: "@alice",
+    body: "This is hands down the best thing I've experienced. Highly recommend!",
+    img: "https://robohash.org/alice",
+  },
+  {
+    name: "Bob",
+    username: "@bob",
+    body: "Incredible work! The attention to detail is phenomenal.",
+    img: "https://robohash.org/bob",
+  },
+  {
+    name: "Charlie",
+    username: "@charlie",
+    body: "This exceeded all my expectations. Absolutely stunning!",
+    img: "https://robohash.org/charlie",
+  },
+  {
+    name: "Dave",
+    username: "@dave",
+    body: "Simply breathtaking. The best decision I've made in a while.",
+    img: "https://robohash.org/dave",
+  },
+  {
+    name: "Eve",
+    username: "@eve",
+    body: "So glad I found this. It has changed the game for me.",
+    img: "https://robohash.org/eve",
+  },
+];
